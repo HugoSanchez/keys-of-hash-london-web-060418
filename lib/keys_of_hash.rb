5e16336e9a9293a09b6633ed5257bde3480330c0
Hugo Sanchez <hugosanchez251@gmail.com>
2018-05-30 09:11:26 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,7 +1,7 @@
 class Hash
   def keys_of(*arguments)
     matches = []
-    Hash.each do |key, value|
+    self.each do |key, value|
       if arguments.include?(value)
         matches << key
       end
